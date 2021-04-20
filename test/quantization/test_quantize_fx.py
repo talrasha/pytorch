849a991b095c57ce9d6d7ee074625cd7f3320edf
Jerry Zhang <jerryzh168@gmail.com>
2021-04-19 17:14:27 -0700

    [quant][graphmode][fx] Produce torch.cat instead of torch.ops.quantized.cat
    Summary:
    Previously we are producing torch.ops.quantize.cat which takes inputs, dequantize them
    and requantize with new qparams. This PR changes that to produce torch.cat directly, torch.cat
    will assume all inputs are sharing the same qparam, and it will produce a quantized Tensor with
    the same qparam as all inputs (because previous PR makes sure all inputs and output of cat are sharing
    the same observer/fakequant instance).
    
    Using torch.cat is expected to be more efficient since it does not introduce extra quant/dequant.
    
    Test Plan:
    python test/test_quantization.py TestQuantizeFx.test_cat
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 563608005c44ac6cc44500c9e7491aa868029137
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/54924

--- test/quantization/test_quantize_fx.py
+++ test/quantization/test_quantize_fx.py
@@ -2873,7 +2873,7 @@ class TestQuantizeFxOps(QuantizationTestCase):
 
         data = (torch.randn(1, 2, 5, 5, dtype=torch.float),
                 torch.randn(1, 2, 5, 5, dtype=torch.float))
-        quantized_node = ns.call_function(torch.ops.quantized.cat)
+        quantized_node = ns.call_function(torch.cat)
         for quant_type in self.static_quant_types:
             self.checkGraphModeFxOp(QuantizedInput(), data, quant_type, quantized_node)
             self.checkGraphModeFxOp(NonQuantizedInput(), data, quant_type, quantized_node)
@@ -2886,7 +2886,9 @@ class TestQuantizeFxOps(QuantizationTestCase):
         all_observers = len(dict(m.named_modules(remove_duplicate=False)))
         distinct_observers = len(dict(m.named_modules()))
         self.assertEqual(all_observers, distinct_observers + 2)
-
+        # make sure the converted model runs
+        m = convert_fx(m)
+        m(*data)
 
     @skipIfNoFBGEMM
     def test_qbatch_norm(self):
@@ -3518,7 +3520,7 @@ class TestQuantizeFxOps(QuantizationTestCase):
                 ns.call_function(torch.ops.quantized.mul),
                 ns.call_function(torch.ops.quantized.mul),
                 ns.call_function(torch.ops.quantized.add_relu),
-                ns.call_function(torch.ops.quantized.cat),
+                ns.call_function(torch.cat),
                 ns.call_method('dequantize')
             ]
             m = convert_fx(m)
