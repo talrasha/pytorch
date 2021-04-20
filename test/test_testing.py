bc68df131e21ea4be004bfcc1fc65294cdbaccac
Philip Meier <github.pmeier@posteo.de>
2021-04-20 07:50:15 +0200

    add support for equal_nan in torch.testing.assert_close
    ghstack-source-id: 694ad2df0a4b03b532441e69f6f20b833826a0e5
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/55788

--- test/test_testing.py
+++ test/test_testing.py
@@ -975,6 +975,23 @@ class TestAsserts(TestCase):
         torch.testing.assert_close(actual, expected, rtol=0.0, atol=eps * 2)
 
     @onlyCPU
+    def test_assert_close_nan(self, device):
+        a = torch.tensor(float("NaN"), device=device)
+        b = torch.tensor(float("NaN"), device=device)
+
+        for inputs in self.make_inputs(a, b):
+            with self.assertRaises(AssertionError):
+                torch.testing.assert_close(*inputs)
+
+    @onlyCPU
+    def test_assert_close_equal_nan(self, device):
+        a = torch.tensor(float("NaN"), device=device)
+        b = torch.tensor(float("NaN"), device=device)
+
+        for inputs in self.make_inputs(a, b):
+            torch.testing.assert_close(*inputs, equal_nan=True)
+
+    @onlyCPU
     def test_mismatching_values_msg(self, device):
         actual = torch.full((3, 3), 5, dtype=torch.float32, device=device)
         expected = actual.clone()
